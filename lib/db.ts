951fdb6a1cdc097f1d53636a7952bf49afea5506
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-02 10:53:15 -0500

    Add startCouchbase and initialisation

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,24 @@
-import { startCouchbase, query } from '@stoqey/serverless-couchbase'
+import { startCouchbase, query } from '@stoqey/serverless-couchbase';
+import get from 'lodash/get';
+
+const connectionString = get(
+  process.env,
+  "COUCHBASE_URL",
+  "couchbase://localhost"
+);
+const bucketName = get(process.env, "COUCHBASE_BUCKET", "stq");
+const username = get(process.env, "COUCHBASE_USERNAME", "admin");
+const password = get(process.env, "COUCHBASE_PASSWORD", "123456");
+
 
 /**
  * Start Couchbase from here
  */
 startCouchbase({
-    connectionString: 'couchbase://localhost',
-    bucketName: 'stq',
-    username: 'admin',
-    password: '123456',
+    connectionString,
+    bucketName,
+    username,
+    password,
 });
 
 export { query }; 
