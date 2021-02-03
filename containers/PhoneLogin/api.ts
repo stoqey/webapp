97e4d80005a758401eaecaad9354dcb8f7671f5e
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-03 03:30:16 -0500

    phoneLoginApi patch

--- containers/PhoneLogin/api.ts
+++ containers/PhoneLogin/api.ts
@@ -1,28 +1,28 @@
 import { ApolloClient } from '@apollo/react-hooks';
-import { log } from '../../config';
-import { LoginResponseType, LOGIN_MUTATION } from '@stoqey/client-graphql';
+import { LoginResponseType, PHONE_LOGIN_MUTATION } from '@stoqey/client-graphql';
 
 interface LoginCreds {
-  password: string;
-  email: string;
+  phone: string;
+  firebaseToken: string;
+  createNew: boolean;
 }
 
-export const loginApi = async ({
+export const phoneLoginApi = async ({
   creds,
   client,
-  errorFunction,
-  successFunction,
+  error,
+  success,
 }: {
   creds: LoginCreds;
   client: ApolloClient<any>;
-  errorFunction?: (error: Error) => Promise<any>;
-  successFunction?: (data: LoginResponseType) => Promise<any>;
+  error?: (error: Error) => Promise<any>;
+  success?: (data: LoginResponseType) => Promise<any>;
 }) => {
-  log.info('loginApi', JSON.stringify(creds));
+  console.log('phoneLoginApi', JSON.stringify(creds));
 
   try {
     const { data: dataResponse }: any = await client.mutate({
-      mutation: LOGIN_MUTATION,
+      mutation: PHONE_LOGIN_MUTATION,
       variables: creds,
       fetchPolicy: 'no-cache',
     });
@@ -33,17 +33,17 @@ export const loginApi = async ({
 
     const { data }: { data?: LoginResponseType } = dataResponse;
 
-    log.info('loginApi response', JSON.stringify(data));
+    console.log('loginApi response', JSON.stringify(data));
 
     if (data.success) {
       //   Successful
-      await successFunction(data);
-      return log.info('Login successful', JSON.stringify(data));
+      await success(data);
+      return console.log('Login successful', JSON.stringify(data));
     }
 
     throw new Error('error logging in, please try again later');
-  } catch (error) {
-    log.error('Error logging in', error);
-    await errorFunction(error);
+  } catch (err) {
+    console.error(err);
+    await error(err);
   }
 };
