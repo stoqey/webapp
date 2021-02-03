699b371fc6eee53e5b891ccba88b55bc51b2c4fe
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-03 04:46:55 -0500

    AsyncStorageDB

---
+++ lib/AsyncStorageDB.ts
@@ -0,0 +1,80 @@
+import isEmpty from 'lodash/isEmpty';
+
+export const USER_DB_PATH = 'USER_DB_PATH';
+
+export interface UserAuthObject {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+
+  accessToken: string;
+  refreshToken: string;
+  expirationTime?: string;
+  balance: number;
+  currency: string;
+}
+
+// Require cycle:
+export const JSONDATA = (data: any): Record<string, any> | string | null => {
+  if (isEmpty(data) || !data) {
+    return null;
+  }
+
+  try {
+    if (typeof data !== 'object') {
+      return JSON.parse(data);
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+    return data;
+  }
+};
+
+
+const getItem =  async (key:string): Promise<any> => {
+  const item = await localStorage.getItem(key);
+  return JSONDATA(item);
+}
+
+const setItem = async function (key: string, value: Record<string, any>) {
+  await null;
+  return localStorage.setItem(key, JSON.stringify(value));
+}
+
+const getAuthItem =  async (): Promise<any> => {
+  const item = await localStorage.getItem(USER_DB_PATH);
+  return JSONDATA(item);
+}
+
+const updateAuthItem = async (data: any) => {
+  try {
+    const existingData = await getAuthItem();
+
+    const newState = {
+      ...(existingData || {}),
+      ...data,
+    };
+  
+    await setItem(USER_DB_PATH, newState);
+
+    return newState;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+const deleteItem = async (path:string): Promise<any> => localStorage.removeItem(path)
+
+export const AsyncStorageDB = {
+  getItem,
+  setItem,
+  getAuthItem,
+  updateAuthItem,
+  deleteItem
+};
+
+export default AsyncStorageDB;
