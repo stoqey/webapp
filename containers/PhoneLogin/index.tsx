c60b72ed501dfadd00b5edcfd7aee47d8af95260
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-02 23:14:31 -0500

    phone login

--- containers/PhoneLogin/index.tsx
+++ containers/PhoneLogin/index.tsx
@@ -1,15 +1,27 @@
-import 'react-phone-number-input/style.css';
-import PhoneInput from 'react-phone-number-input'
+import React from 'react';
+import firebaseConfig from 'keys/config';
+import * as firebaseui from "firebaseui";
+import firebase from "firebase";
+interface Props {
 
+};
 
-export function PhoneLogin() {
-  // `value` will be the parsed phone number in E.164 format.
-  // Example: "+12133734253".
-  const [value, setValue] = useState()
+export const PhoneLogin = (props: Props) => {
+
+  React.useEffect(() => {
+    const fbase = firebase.initializeApp(firebaseConfig);
+    const uiConfig = {
+      signInSuccessUrl: "https://netflix-clone-ankur.herokuapp.com/", //This URL is used to return to that page when we got success response for phone authentication.
+      signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
+      tosUrl: "https://netflix-clone-ankur.herokuapp.com/"
+    };
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }, []);
+
   return (
-    <PhoneInput
-      placeholder="Enter phone number"
-      value={value}
-      onChange={setValue}/>
+    <div id="firebaseui-auth-container"></div>
   )
 }
+
+export default PhoneLogin;
