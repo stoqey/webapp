be754177aec8b267c9438eb9403f8ffdfe7ebcaa
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-25 00:40:48 -0500

    OrderBookContainer

---
+++ containers/OrderBook/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import OrderBook from '@/components/OrderBook';
+
+interface Props {
+};
+
+
+export const OrderBookContainer = (props: Props) => {
+    // Use quote query
+    // Use get orders query
+    // Listen for all order changes
+    const { } = props;
+
+    return <OrderBook />;
+}
