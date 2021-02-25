24e5ceb8fca1196cbb126880b30b0ed4d612c41f
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-25 07:12:09 -0500

    edit order book

--- containers/OrderBook/index.tsx
+++ containers/OrderBook/index.tsx
@@ -2,19 +2,10 @@ import React from 'react';
 import _get from 'lodash/get';
 import OrderBook from '@/components/OrderBook';
 import gql from 'graphql-tag';
-import { GET_ALL_ORDERS, OrderType } from '@stoqey/client-graphql';
+import { GET_ALL_ORDERS,ALL_ORDERS_SUBSCRIPTION, OrderType } from '@stoqey/client-graphql';
 import { useApolloClient, useQuery } from '@apollo/client';
 import { isEmpty } from 'lodash';
 
-const ALL_ORDERS_SUBSCRIPTION = gql`
-    subscription SubAllOrders {
-        data: onOrders {
-            id
-            qty
-            price
-        }
-    }
-`;
 
 export const OrderBookContainer = () => {
 
@@ -23,8 +14,8 @@ export const OrderBookContainer = () => {
         fetchPolicy: 'network-only'
     });
     const defualOrders = _get(initData, 'data', []);
-    const [orders, setOrders] = React.useState<OrderType[]>(defualOrders);
-    console.log('initial data is ', defualOrders);
+    const [orders, setOrders] = React.useState<OrderType[]>();
+    // console.log('initial data is ', defualOrders);
 
      // Use quote query
     // Use get orders query
@@ -38,7 +29,7 @@ export const OrderBookContainer = () => {
         })
         const results = subscription.subscribe(data => {
             const dataToSend = _get(data, 'data.data', []);
-            console.log('subscription.subscribe ', dataToSend);
+            // console.log('subscription.subscribe ', dataToSend);
             const parsedData = dataToSend.map(i => ({
                 ...i,
                 date: new Date(i.date)
