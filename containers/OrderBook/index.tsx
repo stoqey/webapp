5e81d1ca3d3f727f4370fd2987a394cb0602818d
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-25 05:54:50 -0500

    OrderBookContainer

--- containers/OrderBook/index.tsx
+++ containers/OrderBook/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import _get from 'lodash/get';
 import OrderBook from '@/components/OrderBook';
+import { GET_ALL_ORDERS, ALL_ORDERS_SUBSCRIPTION, OrderType } from '@stoqey/client-graphql';
+import { useApolloClient } from '@apollo/client';
 
-interface Props {
-};
 
+export const OrderBookContainer = () => {
 
-export const OrderBookContainer = (props: Props) => {
+    const [orders, setOrders ] = React.useState<OrderType[]>([]);
+
     // Use quote query
     // Use get orders query
     // Listen for all order changes
-    const { } = props;
 
-    return <OrderBook />;
+    const client = useApolloClient();
+    React.useEffect(() => {
+
+        const subscription = client.subscribe({
+            query: ALL_ORDERS_SUBSCRIPTION,
+            variables: {},
+            fetchPolicy: 'network-only'
+        })
+        const results = subscription.subscribe(data => {
+            const dataToSend = _get(data, 'data.data', []);
+            const parsedData = dataToSend.map(i => ({
+                ...i,
+                date: new Date(i.date)
+            }));
+
+            setOrders(parsedData);
+        });
+
+        return () => { results.unsubscribe() }
+
+    }, []);
+
+    return <OrderBook orders={orders} />;
 }
