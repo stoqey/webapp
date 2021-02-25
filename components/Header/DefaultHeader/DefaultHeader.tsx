133af18a2d852db3a6dcd38a3ff47c1366727653
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-25 02:55:56 -0500

    Add currency to box

--- components/Header/DefaultHeader/DefaultHeader.tsx
+++ components/Header/DefaultHeader/DefaultHeader.tsx
@@ -24,6 +24,8 @@ const DefaultHeader: React.FC<{}> = () => {
 		<HeaderWrapper className="default">
 			<Container>
 				<TopBar className="top-bar">
+
+					{/* Stoqey logo */}
 					<Logo
 						path="/"
 						src={
@@ -60,11 +62,21 @@ const DefaultHeader: React.FC<{}> = () => {
 								<Logo
 									path="/"
 									src={
-										<SvgIcon
-											src={require('../../../assets/images/logo.svg?include')}
-										/>
+										<div style={{ display: 'flex', justifyContent: 'center' }}>
+											<div style={{ textAlign: 'center', padding: '4px' }}>
+
+												{/*  */}
+												<div style={{ display: 'flex' }}>
+													<h1>$123.5</h1>
+													<p style={{ fontSize: '12px' }}>USD</p>
+												</div>
+
+												<p style={{ color: 'red' }}>-0.51 (0.40%)</p>
+											</div>
+										</div>
 									}
 								/>
+
 								<BetaMenu className="main-nav" />
 							</NavLeft>
 							<NavRight className="cart-and-avatar">
