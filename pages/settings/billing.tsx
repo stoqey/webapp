6022f47207a067d3d12436d87bf216e7698ad8a4
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-04 23:26:54 -0500

    fix paths

--- pages/settings/billing.tsx
+++ pages/settings/billing.tsx
@@ -11,7 +11,7 @@ import SvgIcon from 'components/UiElements/SvgIcon/SvgIcon';
 import { TextButton } from 'components/PageStyles/Settings.styled';
 import { StyledTableHeadAlt } from 'components/PageStyles/Apps.styled';
 
-import billingPageData from '../../../data/billingPage';
+import billingPageData from 'data/billingPage';
 
 const Billing: NextPage<{}> = () => {
 	return (
