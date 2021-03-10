f94256d48962178ff40dfec842b449812c44c76d
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-10 18:44:38 -0500

    OrdersTable

--- components/OrderBook/OrdersTable.tsx
+++ components/OrderBook/OrdersTable.tsx
@@ -13,49 +13,53 @@ interface Props {
 }
 const OrdersTable: NextPage<{ orders }> = ({ orders }: Props) => {
 
-	return (
-		<>
-			<Block
-				paddingTop={['10px', '20px', '30px', '0']}
-				overrides={{ Block: { style: { minHeight: '150px' } } }}
-			>
-				<StyledTable $gridTemplateColumns="max-content auto auto auto max-content">
-					<StyledTableHeadAlt>ID</StyledTableHeadAlt>
-					<StyledTableHeadAlt>Date</StyledTableHeadAlt>
-					<StyledTableHeadAlt>Payment method</StyledTableHeadAlt>
-					<StyledTableHeadAlt>Amount</StyledTableHeadAlt>
-					<StyledTableHeadAlt>Receipt</StyledTableHeadAlt>
-					{orders.map((item, index) => {
-						const striped = index % 2 === 0;
-						return (
-							<Fragment key={index}>
-								<StyledBodyCell $striped={striped}>
-									<SvgIcon
-										src={require('assets/images/check.svg?include')}
-									/>{' '}
-									{item.id}
-								</StyledBodyCell>
-								<StyledBodyCell $striped={striped}>
-									{item.createdAt}
-								</StyledBodyCell>
-								<StyledBodyCell $striped={striped}>
-									{item.source}
-								</StyledBodyCell>
-								<StyledBodyCell $striped={striped}>
-									{item.amount}
-								</StyledBodyCell>
-								<StyledBodyCell $striped={striped}>
-									<TextButton onClick={() => alert('click')}>
-										<MdCloudDownload size="1.2rem" color="#545454" />
-									</TextButton>
-								</StyledBodyCell>
-							</Fragment>
-						);
-					})}
-				</StyledTable>
-			</Block>
-		</>
-	);
+    return (
+        <>
+            <Block
+                paddingTop={['10px', '20px', '30px', '0']}
+                overrides={{ Block: { style: { minHeight: '150px' } } }}
+            >
+                <StyledTable $gridTemplateColumns="max-content auto auto auto max-content">
+                    <StyledTableHeadAlt>Symbol</StyledTableHeadAlt>
+                    <StyledTableHeadAlt>Actn</StyledTableHeadAlt>
+                    <StyledTableHeadAlt>Type</StyledTableHeadAlt>
+                    <StyledTableHeadAlt>Qty</StyledTableHeadAlt>
+                    <StyledTableHeadAlt>Filled</StyledTableHeadAlt>
+                    <StyledTableHeadAlt></StyledTableHeadAlt>
+                    {orders.map((item, index) => {
+                        const striped = index % 2 === 0;
+                        return (
+                            <Fragment key={index}>
+                                <StyledBodyCell $striped={striped}>
+                                    <SvgIcon
+                                        src={require('assets/images/check.svg?include')}
+                                    />{' '}
+                                    {item.instrument}
+                                </StyledBodyCell>
+                                <StyledBodyCell $striped={striped}>
+                                    {item.action}
+                                </StyledBodyCell>
+                                <StyledBodyCell $striped={striped}>
+                                    {item.type}
+                                </StyledBodyCell>
+                                <StyledBodyCell $striped={striped}>
+                                    {item.qty}
+                                </StyledBodyCell>
+                                <StyledBodyCell $striped={striped}>
+                                    {item.qty - item.filledQty}
+                                </StyledBodyCell>
+                                <StyledBodyCell $striped={striped}>
+                                    <TextButton onClick={() => alert('click')}>
+                                        <MdCloudDownload size="1.2rem" color="#545454" />
+                                    </TextButton>
+                                </StyledBodyCell>
+                            </Fragment>
+                        );
+                    })}
+                </StyledTable>
+            </Block>
+        </>
+    );
 };
 
 export default OrdersTable;
