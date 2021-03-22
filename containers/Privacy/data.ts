3435751d7f9bf562217fe1c446d26c5ede18d08f
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-22 01:14:13 -0400

    Privacy completed

--- containers/Privacy/data.ts
+++ containers/Privacy/data.ts
@@ -22,21 +22,143 @@ const termsPageData = {
     },
     {
       id: "4",
-      title: "Accessing your Personal Data",
-      description:
-        "<p>You may request access to your personal information collected by us, and ask that we correct that personal information. You can ask for access or correction by contacting us and we will usually respond within 30 days. If we refuse to give you access to, or correct, your personal information, we will notify you in writing setting out the reasons.</p>",
+      title: "THIRD-PARTY LINKS.",
+      description: `<p>
+        Some of the Services may contain links to content maintained by third parties that we don’t
+        control. We’re not responsible for the privacy practices of these third parties or the content on the
+        third-party services, and the information practices of these third parties are not covered by this
+        Policy. Please note that these third-party services have their own privacy policies, and we
+        strongly recommend that you read their privacy policies as well as terms and conditions of use to
+        understand how they collect, use, and share your information.
+        </p>`,
     },
     {
       id: "5",
-      title: "Complaints",
-      description:
-        "<p>If you believe your privacy has been breached or you have a complaint about how we have handled your personal information, please contact us in writing.  We will respond within a reasonable period (usually within 30 days).</p>",
+      title: "THIRD-PARTY TRACKING AND ONLINE ADVERTISING",
+      description: `
+        <p>
+        We participate in interest-based advertising and use third-party advertising companies to serve
+        you targeted advertisements based on your online browsing history and your interests. As a
+        self-directed broker-dealer, we do not advertise on behalf of specific securities or investment
+        options on our Services. We permit third-party online advertising networks, social media
+        companies, and other third-party services to collect information about your use of our Services
+        over time so that they may play or display ads on other websites, apps, or services you may use,
+        and on other devices you may use. Typically, though not always, the information used for
+        interest-based advertising is collected through cookies or similar tracking technologies. For
+        advertising, we may share a common account identifier (such as an email address or user ID) or
+        hashed data with our third-party advertising partners to help identify you across devices. We and
+        our third-party partners use this information to make the advertisements you see online on other
+        platforms more relevant to your interests, as well as to provide advertising-related services such
+        as reporting, attribution, analytics, and market research.
+        </p>
+
+        <p>
+        We’re not responsible for the privacy practices of these third parties, and the information
+        practices of these third parties are not covered by this Policy. Some of these companies are
+        members of the Network Advertising Initiative, which offers a single location to opt out of ad
+        targeting from member companies. To learn more, please visit
+        http://www.networkadvertising.org/choices or the DAA’s resources, available at
+        http://www.aboutads.info/choices . You may also be able to set your browser to delete or notify
+        you of cookies by actively managing the settings on your browser or mobile device. Please note
+        that some advertising opt-outs may not be effective unless your browser is set to accept cookies.
+        Furthermore, if you use a different device, change browsers, or delete the opt-out cookies, you
+        may need to perform the opt-out task again. You may also be able to limit certain interest-based
+        mobile advertising through the settings on your mobile device by selecting “limit ad tracking”
+        (iOS) or “opt out of interest based ads” (Android).
+        </p>
+
+        <p>
+          Google Analytics and Advertising . We use Google Analytics to recognize you and link your devices
+          and when you use our Services on your browser or mobile device, log in to your account on our
+          Services, or otherwise engage with us. We share a unique identifier, like a user ID or hashed email
+          address, with Google to facilitate the Services. Google Analytics allows us to better understand
+          how our users interact with our Services and to tailor our advertisements and content to you. For
+          information on how Google Analytics collects and processes data, as well as how you can control
+          information sent to Google, see “How Google uses data when you use our partners’ sites or apps”
+          linked here: http://www.google.com/policies/privacy/partners/ . You can learn about Google
+          Analytics’ currently available opt-outs, including the Google Analytics Browser Ad-On, here:
+          https://tools.google.com/dlpage/gaoptout/ . We may also use certain forms of display advertising
+          and other advanced features through Google Analytics, such as Remarketing with Google
+          Analytics, Google Display Network Impression Reporting, the DoubleClick Campaign Manager
+          Integration, and Google Analytics Demographics and Interest Reporting. These features let us to
+          use first-party cookies (such as the Google Analytics cookie) and third-party cookies (such as the
+          DoubleClick advertising cookie) or other third-party cookies together to inform, optimize, and
+          display ads based on your use of our Services. You may control your advertising preferences or
+          opt-out of certain Google advertising products by visiting the Google Ads Preferences Manager,
+          currently available at https://google.com/ads/preferences , or by visiting NAI’s online resources at
+          http://www.networkadvertising.org/choices .
+        </p>
+        `,
     },
     {
       id: "6",
-      title: "Owner and Data Controller",
+      title: "CONTROL OVER YOUR INFORMATION",
+      description: `
+        <strong>Account profile.</strong>  You may update certain of your account profile information by logging into your
+        account. <br/>
+
+        <strong>Access to your device data</strong> . You may control the mobile app’s access to your device information
+        in your device’s settings. For instance, you can withdraw permission for the app to access your
+        contact list or photo gallery. <br/>
+
+        <strong>How to control your communications preferences</strong> . You can stop receiving promotional emails
+        from us by clicking on the provided “unsubscribe” link. You may not opt out of service-related or
+        other non-promotional communications (e.g., account verification, transactional
+        communications, changes/updates to features of the Services, and technical and security
+        notices). <br/>
+
+        <strong>Affiliate Sharing Opt Out</strong> . In some cases, you can limit sharing between Stoqey affiliates.
+        Please see our Consumer Privacy Notice for more details. If you want to exercise this right, please
+        contact at glba-request@stoqey.com .
+      `,
+    },
+    {
+      id: "7",
+      title: "TRANSFER OF PERSONAL INFORMATION.",
+      description: `
+        <p>
+        Our Services are currently designed for use only in certain select markets. If you are using our
+        Services from another jurisdiction, your information collected through our Services may be stored
+        and processed in any country in which Stoqey or its parent,
+        subsidiaries, affiliates, or service providers maintain facilities or conduct business activities. If
+        you are located in other regions with laws governing data collection and use that may differ from
+        U.S. law, please note that we may transfer information, including personal information, to a
+        country and jurisdiction that does not have the same data-protection laws as your jurisdiction.
+        </p>
+        `,
+    },
+    {
+      id: "8",
+      title: "CHILDREN’S PRIVACY",
       description:
-        "<p>The Commons<br>20-40 Meagher St,<br>Chippendale NSW 2008<br>Australia<br><br>Email: hello@littleandbig.com.au</p>",
+        `
+        <p>
+        We do not knowingly collect or solicit any information from anyone under the age of 13 on these
+        Services. In the event that we learn that we have inadvertently collected personal information
+        from a child under age 13, we will take reasonable steps to delete that information. If you believe
+        that we might have any information from a child under 13, please contact us at
+        privacy@stoqey.com.
+        </p>
+        `,
+    },
+    {
+      id: "9",
+      title: "CHANGES TO THIS POLICY",
+      description: `
+      <p>
+      This Privacy Policy will evolve with time, and when we update it, we will revise the
+      "Effective Date" above and post the new Policy. To stay informed of our privacy practices, we
+      recommend you review the Policy on a regular basis as you continue to use our Services.
+      </p>
+      `,
+    },
+    {
+      id: "10",
+      title: "HOW TO CONTACT US",
+      description: `
+      If you have any questions about this Privacy Policy or the Services, please contact
+      us at privacy@stoqey.com.
+      `,
     },
   ],
 };
