53369b72dad9c2d7e6b1db6e53db062a87256419
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-22 22:21:43 -0400

    changePct

--- components/Currency/currencyNumber.tsx
+++ components/Currency/currencyNumber.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { styled } from 'baseui';
-import { H2, H3, Paragraph1 } from 'baseui/typography';
+import { H3, Paragraph1 } from 'baseui/typography';
 import { niceDec } from 'utils/number';
+import { getTradeColor } from 'utils/colors';
 
 interface Props {
 	close: number;
 	change: number;
-	changePtc: number;
+	changePct: number;
 };
 export const CurrencyNumber = (props: Props) => {
-	const { close, change, changePtc } = props;
-	const changeStyleColor = changePtc >= 0 ? 'green' : 'red';
-
-	const changeSign = changePtc >= 0 ? '+' : '-';
+	const { close, change, changePct } = props;
+	const changeStyleColor = getTradeColor(changePct);
 
 	return (
 		<div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -21,8 +19,8 @@ export const CurrencyNumber = (props: Props) => {
 					<H3>${niceDec(close)}</H3>
 					<Paragraph1>USD</Paragraph1>
 				</div>
-				 
-				<p style={{ color: changeStyleColor }}>{changeSign}{niceDec(change)} ({niceDec(changePtc)}%)</p>
+
+				<p style={{ color: changeStyleColor }}>{niceDec(change)} ({changePct}%)</p>
 			</div>
 		</div>
 	)
