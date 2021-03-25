0a8d557a5d4f40d03b8ee497cce8d21f9ffc1f4d
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-24 23:22:03 -0400

    useLocalStorage

--- hooks/useUserInfo.tsx
+++ hooks/useUserInfo.tsx
@@ -1,22 +1,40 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { LoginResponseType } from '@stoqey/client-graphql'
 import AsyncStorageDB from 'lib/AsyncStorageDB';
 import { APPEVENTS, AppEvents } from '@/lib/AppEvent';
+import { useAppEvent } from './useAppEvent';
 
 /**
  * Use userInfo
  */
-export function useUserInfo(): LoginResponseType {
-  const db = AsyncStorageDB;
+// export function useUserInfo(): LoginResponseType {
+//   const db = AsyncStorageDB;
 
+//   const [userInfo, setUserInfo] = useState<LoginResponseType>(null);
+
+//   useEffect(() => {
+//     if (!userInfo) {
+//       db.getAuthItem().then(dbData => setUserInfo(dbData));
+//     }  
+//   }, [userInfo])
+
+//   return !userInfo ? ({} as any) : userInfo;
+// }
+
+export function useUserInfo(): LoginResponseType {
+  const appEventUserInfo = useAppEvent(APPEVENTS.AUTH)
   const [userInfo, setUserInfo] = useState<LoginResponseType>(null);
 
   useEffect(() => {
-    if (!userInfo) {
-      db.getAuthItem().then(dbData => setUserInfo(dbData));
-    }  
-  }, [userInfo])
-  
+    if (!appEventUserInfo) {
+      fetchUserInfo();
+    } else {
+      if(!userInfo){
+        setUserInfo(appEventUserInfo)
+      }
+    }
+  }, [appEventUserInfo])
+
   return !userInfo ? ({} as any) : userInfo;
 }
 
