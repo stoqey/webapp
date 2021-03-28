e3a3d2b75a8ce2458bdf3f064bba69c3eec3dc5d
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-27 22:27:34 -0400

    cancel order

--- components/OrderBook/OrdersTable.tsx
+++ components/OrderBook/OrdersTable.tsx
@@ -8,36 +8,43 @@ import SvgIcon from 'components/UiElements/SvgIcon/SvgIcon';
 import { TextButton } from 'components/PageStyles/Settings.styled';
 import { StyledTableHeadAlt } from 'components/PageStyles/Apps.styled';
 import { isEmpty } from 'lodash';
+import { useUserInfo } from 'hooks/useUserInfo';
+import { Button } from 'baseui/button';
+import { ImCross } from 'react-icons/im';
 const stoqeyLogo = require('assets/images/STQ.png');
 
 interface Props {
     orders: OrderType[]
 }
 const OrdersTable: NextPage<Props> = ({ orders }: Props) => {
+    const { user } = useUserInfo();
+    const userId = user && user.id;
 
     return (
         <>
-            
+
             <Block
                 paddingTop={['10px', '20px', '30px', '0']}
                 overrides={{ Block: { style: { minHeight: '150px' } } }}
             >
-                
+
                 <StyledTable $gridTemplateColumns="max-content auto auto auto max-content">
-                    <StyledTableHeadAlt>Symbol</StyledTableHeadAlt>
+                    {/* <StyledTableHeadAlt>Symbol</StyledTableHeadAlt> */}
                     <StyledTableHeadAlt>Actn</StyledTableHeadAlt>
                     <StyledTableHeadAlt>Type</StyledTableHeadAlt>
                     <StyledTableHeadAlt>Qty</StyledTableHeadAlt>
                     <StyledTableHeadAlt>Filled</StyledTableHeadAlt>
+                    <StyledTableHeadAlt></StyledTableHeadAlt>
                     {/* <StyledTableHeadAlt></StyledTableHeadAlt> */}
                     {!isEmpty(orders) && orders.map((item, index) => {
+                        const { owner } = item;
+                        const isMine = owner === userId;
                         const striped = index % 2 === 0;
                         return (
                             <Fragment key={index}>
-                                
-                                <StyledBodyCell $striped={striped}>
+                                {/* <StyledBodyCell $striped={striped}>
                                     {item.instrument}
-                                </StyledBodyCell>
+                                </StyledBodyCell> */}
                                 <StyledBodyCell $striped={striped}>
                                     {item.action}
                                 </StyledBodyCell>
@@ -50,11 +57,17 @@ const OrdersTable: NextPage<Props> = ({ orders }: Props) => {
                                 <StyledBodyCell $striped={striped}>
                                     {item.qty - item.filledQty}
                                 </StyledBodyCell>
-                                {/* <StyledBodyCell $striped={striped}>
-                                    <TextButton onClick={() => alert('click')}>
-                                        <MdCloudDownload size="1.2rem" color="#545454" />
-                                    </TextButton>
-                                </StyledBodyCell> */}
+
+                                {isMine ? (
+                                    <StyledBodyCell $striped={striped}>
+                                        <Button shape="round" kind="tertiary" onClick={() => { }}>
+                                            <ImCross color="red" />
+                                        </Button>
+                                    </StyledBodyCell>
+                                ) : (
+                                    <div />
+                                )}
+
                             </Fragment>
                         );
                     })}
