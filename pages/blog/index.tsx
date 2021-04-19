e0d2504b85a09d9f2ef8c22a4e98fb4c5cd26259
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-19 02:59:28 -0400

    index blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { Grid, Cell } from 'baseui/layout-grid';
-import { useQuery, gql } from '@apollo/client';
-import { Block } from 'baseui/block';
-import { Button } from 'baseui/button';
-import Container from 'components/UiElements/Container/Container';
 import PageTitle from 'components/UiElements/PageTitle/PageTitle';
 import ProductCard from 'components/UiElements/ProductCard/ProductCard';
 
-const TITLE = 'Shopping Store';
-const SUB_TITLE = 'Awesome GitHub T-shirts and other cool swag';
+const TITLE = 'Stoqey Blog';
+const SUB_TITLE = 'Stoqey Blog';
 
 const Shop: NextPage<{}> = () => {
 
 	return (
 		<>
 			<Head>
-				<title>Shop | INST.</title>
-				<meta name="Description" content="Inst shop page" />
+				<title>Stoqey Blogs.</title>
+				<meta name="Description" content="Stoqey Blog" />
 			</Head>
 
 			<PageTitle title={TITLE} subtitle={SUB_TITLE} />
-
-			<Container>
-				<Block paddingBottom="20px">
-					<img
-						src={require('../../assets/images/shop-banner.png')}
-						width="100%"
-						alt="Banner"
-					/>
-				</Block>
-			</Container>
 		</>
 	);
 };
