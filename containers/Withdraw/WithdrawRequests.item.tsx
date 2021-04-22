5ece9eec70a877a0a0d2ea1d10dc8b61263d79e2
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-22 03:13:25 -0400

    getStatusText

--- containers/Withdraw/WithdrawRequests.item.tsx
+++ containers/Withdraw/WithdrawRequests.item.tsx
@@ -1,30 +1,61 @@
+import { WithdrawRequestType, StatusType } from '@stoqey/client-graphql';
 import { Block } from 'baseui/block';
 import { Button } from 'baseui/button';
 import { H3, Paragraph3 } from 'baseui/typography';
 import React from 'react';
 import { ImCross } from 'react-icons/im';
+import { TiArrowSyncOutline } from 'react-icons/ti';
+import { BsArrowClockwise } from 'react-icons/bs';
+import { FaCheck } from 'react-icons/fa';
 
-export const WithdrawRequestItem = () => {
-    return <div style={{ textAlign: "center" }}>
+
+const getStatusText = (status: StatusType): any[] => {
+    switch (status) {
+        case StatusType.PROCESSING:
+            return ["Processing transaction", "green", BsArrowClockwise];
+        case StatusType.REJECTED:
+            return ["Processing transaction", "green", BsArrowClockwise];
+        case StatusType.FAIL:
+            return ["Processing transaction", "red", BsArrowClockwise];
+        case StatusType.DRAFT:
+            return ["Processing transaction", "red", BsArrowClockwise];
+        case StatusType.PENDING:
+            return ["some success string", "orange", BsArrowClockwise];
+        case StatusType.SUCCESS:
+            return ["successfully processed", "green", FaCheck];
+        default:
+            return ["some default string", "blue", ImCross];
+    }
+};
+
+export const WithdrawRequestItem = (props: WithdrawRequestType) => {
+    const { status, amount } = props;
+
+    const [statusText, color, Icon] = getStatusText(status);
+
+    const cannotBeCanceled = [StatusType.REJECTED, StatusType.FAIL, StatusType.SUCCESS].includes(status);
+
+    return <div style={{ textAlign: "center", border: "black solid 0.5px", margin: "2px", padding: "10px" }}>
 
         <Paragraph3>Some status about the transaction</Paragraph3>
 
         <Block display="flex" width="100%" justifyContent="space-between">
 
-            <Button shape="round" $style={{ backgroundColor: 'blueviolet' }}
+            <Button shape="round" $style={{ backgroundColor: color }}
                 onClick={() => { }}>
-                <ImCross />
+                <Icon />
             </Button>
 
-            <H3> 1, 000</H3>
-            <Button shape="round" $style={{ backgroundColor: 'red' }}
+            <H3>{amount}</H3>
+
+            <Button disabled={cannotBeCanceled} shape="round" $style={{ backgroundColor: 'red' }}
                 onClick={() => { }}>
                 <ImCross />
             </Button>
 
         </Block>
 
-        <Paragraph3>Some status about the transaction</Paragraph3>
+        <Paragraph3>{statusText}</Paragraph3>
 
     </div>
 }
--- containers/Withdraw/WithdrawRequests.lists.tsx
+++ containers/Withdraw/WithdrawRequests.lists.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import WithdrawRequestItem from './WithdrawRequests.item';
+import { WithdrawRequestType, StatusType } from '@stoqey/client-graphql';
+
+const requests: WithdrawRequestType[] = [
+    {
+        id: "1",
+        owner: "ceddy",
+        amount: 1000,
+        status: StatusType.SUCCESS,
+    },
+    {
+        id: "2",
+        owner: "ceddy",
+        amount: 400,
+        status: StatusType.PENDING,
+    }
+]
 
 export const WithdrawRequestList = () => {
     return (
         <div>
-            <WithdrawRequestItem />
-            <WithdrawRequestItem />
-            <WithdrawRequestItem />
+            {requests.map((i) => <WithdrawRequestItem key={i.id} {...i} />)}
         </div>
     )
 }
