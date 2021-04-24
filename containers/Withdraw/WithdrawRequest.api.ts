25366d7866f1ccfb1e8db239453b3f83ff686003
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-24 15:51:07 -0400

    createUpdateWithdrawRequestMutation

--- containers/Withdraw/WithdrawRequest.api.ts
+++ containers/Withdraw/WithdrawRequest.api.ts
@@ -54,3 +54,43 @@ export const getWithdrawRequestsPaginationApi = async ({
     error && await error(err);
   }
 };
+
+export const createUpdateWithdrawRequestMutation = async ({
+  args,
+  client,
+  error,
+  success,
+}: {
+  args: WithdrawRequestType;
+  client: ApolloClient<any>;
+  error?: (error: Error) => Promise<any>;
+  success?: (data: any) => Promise<any>;
+}) => {
+  console.log('processPayment', JSON.stringify(args));
+
+  try {
+    const { data: dataResponse }: any = await client.mutate({
+      mutation: CREATE_WITHDRAWREQUEST_MUTATION,
+      variables: args,
+      fetchPolicy: 'no-cache',
+    });
+
+    if (!dataResponse) {
+      throw new Error('error processing payment data');
+    }
+
+    const { data }: { data?: ResType } = dataResponse;
+    if (data.success) {
+      await success(data);
+      return;
+    }
+
+    throw new Error('error processing payment, please try again later');
+  } catch (err) {
+    console.error(err);
+    await error(err);
+  }
+};
+
+// TODO accept/reject
+// TODO delete
