4fed18aa041f5f46456ed2203d23f7659165b046
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-30 10:14:51 -0400

    WithdrawRequestList

--- containers/Withdraw/WithdrawRequests.lists.tsx
+++ containers/Withdraw/WithdrawRequests.lists.tsx
@@ -1,54 +1,26 @@
 import React from 'react';
 import WithdrawRequestItem from './WithdrawRequests.item';
 import { WithdrawRequestType, StatusType } from '@stoqey/client-graphql';
+import { getWithdrawRequestsPaginationApi } from './WithdrawRequest.api'
+import { useApolloClient } from '@apollo/react-hooks';
 
-const requests: WithdrawRequestType[] = [
-    {
-        id: "0",
-        owner: "ceddy",
-        amount: 1000,
-        status: StatusType.DRAFT,
-        notes: "Draft"
-    },
-    // {
-    //     id: "1",
-    //     owner: "ceddy",
-    //     amount: 1000,
-    //     status: StatusType.SUCCESS,
-    //     notes: "Success"
-    // },
-    // {
-    //     id: "2",
-    //     owner: "ceddy",
-    //     amount: 400,
-    //     status: StatusType.PENDING,
-    //     notes: "Pending"
-    // },
-    // {
-    //     id: "3",
-    //     owner: "ceddy",
-    //     amount: 400,
-    //     status: StatusType.FAIL,
-    //     notes: "Failed"
-    // },
-    // {
-    //     id: "4",
-    //     owner: "ceddy",
-    //     amount: 400,
-    //     status: StatusType.REJECTED,
-    //     notes: "Rejected"
-    // },
-    // {
-    //     id: "5",
-    //     owner: "ceddy",
-    //     amount: 400,
-    //     status: StatusType.PROCESSING,
-    //     notes: "Processing"
-    // }
-]
 
 export const WithdrawRequestList = () => {
-    
+
+    const [requests, setRequests] = React.useState<WithdrawRequestType[]>();
+
+    const client = useApolloClient();
+
+    const getDataApi = () => getWithdrawRequestsPaginationApi({
+        client,
+        args: {},
+        error: async () => {
+
+        },
+        success: async (data) => setRequests(data)
+    })
+
+
     return (
         <div>
             {requests.map((i, index) => <WithdrawRequestItem key={`${index}-${i.id}`} {...i} />)}
