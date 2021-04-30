b803a55b5384599f119a5fdb508dfba94ddd4183
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-29 23:22:12 -0400

    add amount

--- containers/Withdraw/WithdrawRequest.form.tsx
+++ containers/Withdraw/WithdrawRequest.form.tsx
@@ -1,68 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'baseui/button';
 import { toaster } from 'baseui/toast';
 import { Modal, ModalHeader, ModalBody } from 'baseui/modal';
 import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
 import { FormControl } from 'baseui/form-control';
 import { Input } from 'baseui/input';
+import { useUserInfo } from 'hooks/useUserInfo';
+import { H4 } from 'baseui/typography';
+import { niceDec } from 'utils/number';
+
+interface State {
+    amount: number;
+}
 
 export const WithdrawForm = () => {
-    return (
-        <>
-            <FlexGrid flexGridColumnCount={1}>
-                <FlexGridItem>
-                    <FormControl
-                        label="Enter amount to withdraw"
-                        // error={error && 'Please fill out balance'}
-                        overrides={{
-                            Label: {
-                                style: ({ $theme }) => {
-                                    return { 
-                                        textAlign: 'center',
-                                        ...$theme.typography.font200
-                                     };
-                                },
+    const { user } = useUserInfo();
+    const balance = user && user.balance || 0;
+    const [state, setState] = useState<State>({ amount: 1 });
+
+    const { amount } = state;
+
+
+    return (<>
+        <FlexGrid flexGridColumnCount={1}>
+            <FlexGridItem>
+                <H4>${niceDec(balance - +amount)}</H4>
+            </FlexGridItem>
+            <FlexGridItem>
+                <FormControl
+                    label="Enter amount to withdraw"
+                    // error={error && 'Please fill out balance'}
+                    overrides={{
+                        Label: {
+                            style: ({ $theme }) => {
+                                return {
+                                    textAlign: 'center',
+                                    ...$theme.typography.font200
+                                };
                             },
-                        }}
-                    >
-                        <Input
-                            type="number"
-                            name="amount"
-                            // value={balance}
-                            // error={error && !balance}
-                            // onChange={handleOnChangeBalance}
-                            overrides={{
-                                InputContainer: {
-                                    style: () => {
-                                        return { backgroundColor: 'transparent' };
-                                    },
-                                },
-                            }}
-                        />
-                    </FormControl>
-                </FlexGridItem>
-
-
-                <FlexGridItem
-                    overrides={{ Block: { style: { marginTop: '10px', justifyContent: "center", display: "flex" } } }}
+                        },
+                    }}
                 >
-                    <Button
-                        // onClick={updateUserBalance}
-                        shape="pill"
+                    <Input
+                        type="number"
+                        name="amount"
+                        value={amount}
+                        error={+amount < 0 || balance < 0}
+                        onChange={(event: any) => setState({ amount: event.target.value})}
                         overrides={{
-                            BaseButton: {
-                                style: ({ $theme }) => {
-                                    return {
-                                        ...$theme.typography.font250,
-                                    };
+                            InputContainer: {
+                                style: () => {
+                                    return { backgroundColor: 'transparent' };
                                 },
                             },
                         }}
-                    >
-                        {'Submit Withdraw request'}
-                    </Button>
-                </FlexGridItem>
-            </FlexGrid>
-        </>
+                    />
+                </FormControl>
+            </FlexGridItem>
+
+
+            <FlexGridItem
+                overrides={{ Block: { style: { marginTop: '10px', justifyContent: "center", display: "flex" } } }}
+            >
+                <Button
+                    // onClick={updateUserBalance}
+                    shape="pill"
+                    overrides={{
+                        BaseButton: {
+                            style: ({ $theme }) => {
+                                return {
+                                    ...$theme.typography.font250,
+                                };
+                            },
+                        },
+                    }}
+                >
+                    {'Submit Withdraw request'}
+                </Button>
+            </FlexGridItem>
+        </FlexGrid>
+    </>
     )
 }
