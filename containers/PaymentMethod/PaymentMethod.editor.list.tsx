cdef1806d66c6497032c7764825a3bb0038c23c9
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-05-21 10:49:56 -0400

    PaymentMethodLists

---
+++ containers/PaymentMethod/PaymentMethod.editor.list.tsx
@@ -0,0 +1,16 @@
+import { PaymentMethodType } from '@stoqey/client-graphql';
+import React from 'react';
+
+
+interface Props {
+    items: PaymentMethodType[];
+    setSelected: (selected: PaymentMethodType) => void
+};
+
+export const PaymentMethodLists = (props: Props) => {
+    return (
+        <>
+
+        </>
+    )
+}
