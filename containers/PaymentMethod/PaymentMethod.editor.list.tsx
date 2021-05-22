ff9ddad758f139cbbc860a5bce4295135d510cc1
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-05-21 21:46:46 -0400

    PaymentMethodLists

--- containers/PaymentMethod/PaymentMethod.editor.list.tsx
+++ containers/PaymentMethod/PaymentMethod.editor.list.tsx
@@ -1,17 +1,59 @@
-import { PaymentMethodType } from '@stoqey/client-graphql';
 import React from 'react';
+import { StatusType, PaymentMethodType, WithdrawPaymentMethodType } from '@stoqey/client-graphql';
+import { Block } from 'baseui/block';
+import { Button } from 'baseui/button';
+import { H3, Paragraph3 } from 'baseui/typography';
+import { ImCross } from 'react-icons/im';
+import { BsArrowClockwise } from 'react-icons/bs';
+import { FaArrowCircleRight, FaCheck, FaEdit } from 'react-icons/fa';
+
+
+const withdrawPaymentTypeObject = {
+    [WithdrawPaymentMethodType.BANK]: ["Draft", "grey", FaEdit],
+    [WithdrawPaymentMethodType.MOBILEMONEY]: ["Pending", "orange", BsArrowClockwise],
+    [WithdrawPaymentMethodType.ETRANSFER]: ["Submitted", "orange", FaArrowCircleRight],
+};
 
 
 interface Props {
     items: PaymentMethodType[];
     setSelected: (selected: PaymentMethodType) => void
+    deleteItem: (selected: PaymentMethodType) => void
 };
 
 export const PaymentMethodLists = (props: Props) => {
-    const { items, setSelected }  = props;
-    return (
-        <>
+    const { items, setSelected, deleteItem } = props;
 
-        </>
+    return (
+        items.map((i) => {
+            const { name, info } = i;
+            const paymentMethodType: WithdrawPaymentMethodType  = i && i.type as WithdrawPaymentMethodType || WithdrawPaymentMethodType.BANK;
+            const [label, color, Icon] = withdrawPaymentTypeObject[paymentMethodType];
+
+            return (
+                <div key={i.id} style={{ textAlign: "center", border: "black solid 0.5px", margin: "2px", padding: "10px" }}>
+                    <Paragraph3>Some status about the transaction</Paragraph3>
+
+                    <Block display="flex" width="100%" justifyContent="space-between">
+
+                        <Button shape="round" $style={{ backgroundColor: color }}
+                            onClick={() => { }}>
+                            <Icon />
+                        </Button>
+
+                        <H3>{name}</H3>
+
+                        <Button disabled={cannotBeCanceled} shape="round" $style={{ backgroundColor: 'red' }}
+                            onClick={() => deleteItem(i)}>
+                            <ImCross />
+                        </Button>
+
+                    </Block>
+
+                    <Paragraph3>{info}</Paragraph3>
+
+                </div>
+            )
+        })
     )
 }
