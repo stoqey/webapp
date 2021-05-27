b56c3d2d4b0f694cc2ceec491a4a079db27aef7d
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-05-27 01:51:08 -0400

    WithdrawConfirmModal

---
+++ containers/Admin/Withdraw/Withdraw.confirm.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Button } from 'baseui/button';
+import { Modal, ModalHeader, ModalBody } from 'baseui/modal';
+import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
+import { FormControl } from 'baseui/form-control';
+import { Input } from 'baseui/input';
+import { Textarea } from 'baseui/textarea';
+import { Select } from 'baseui/select';
+import { StatusType, UserType, WithdrawRequestType } from '@stoqey/client-graphql';
+
+interface Props {
+  withdrawRequest: WithdrawRequestType,
+  newStatus: StatusType,
+  visible: boolean,
+  handleOnSubmit: Function,
+  handleModdalClose: Function
+}
+const AddEditModal = ({
+  withdrawRequest,
+  newStatus,
+  visible,
+  handleOnSubmit,
+  handleModdalClose,
+}: Props) => {
+  return (
+    <>
+      <Modal
+        // @ts-ignore
+        onClose={handleModdalClose}
+        closeable
+        isOpen={visible}
+        animate
+        size="default"
+        role="dialog"
+        unstable_ModalBackdropScroll={true}
+        overrides={{
+          Root: {
+            style: () => {
+              return { zIndex: 9999 };
+            },
+          },
+        }}
+      >
+        <ModalHeader>
+          {'Confirm withdraw request'}
+        </ModalHeader>
+
+        <ModalBody style={{ overflow: 'hidden' }}>
+          <FlexGridItem
+            overrides={{ Block: { style: { marginTop: '30px' } } }}
+          >
+            <Button
+              // @ts-ignore
+              onClick={handleOnSubmit}
+              overrides={{
+                BaseButton: {
+                  style: ({ $theme }) => {
+                    return {
+                      ...$theme.typography.font250,
+                    };
+                  },
+                },
+              }}
+            >
+              {'Confirm withdraw'}
+            </Button>
+            <Button
+              kind="minimal"
+              // @ts-ignore
+              onClick={handleModdalClose}
+              overrides={{
+                BaseButton: {
+                  style: ({ $theme }) => {
+                    return {
+                      ...$theme.typography.font250,
+                    };
+                  },
+                },
+              }}
+            >
+              Cancel
+              </Button>
+          </FlexGridItem>
+          
+        </ModalBody>
+    </Modal>
+    </>
+  );
+};
+
+export default AddEditModal;
