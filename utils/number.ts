43a77378581f960af00ca5cefd168e73e42d9922
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-06-08 23:30:04 -0400

    niceDec

--- utils/number.ts
+++ utils/number.ts
@@ -1,11 +1,14 @@
-
 /**
  * Nice Decimal number
- * @param num 
+ * @param num
  */
-export const niceDec = (num: number = 0): string => {
-    if(typeof num !== "number"){
-        return ``;
+export const niceDec = (num: number = 0, showSign: boolean = true): string => {
+  const getNumber = () => {
+    if (typeof num !== "number") {
+      return ``;
     }
-    return (num).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-}
+    return num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+  };
+
+  return `${showSign? "$": ""}${getNumber()}`
+};
