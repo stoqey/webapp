561cc8e3d1b6db164d6c4eb0f6c38e2e2e1ae4d2
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-06-09 23:38:18 -0400

    adminFetchPaymentMethods

--- containers/Admin/admin.api.tsx
+++ containers/Admin/admin.api.tsx
@@ -1,4 +1,4 @@
-import { GET_ALL_USERS, UserType } from '@stoqey/client-graphql';
+import { GET_ALL_USERS, GET_PAYMENT_METHODS, UserType } from '@stoqey/client-graphql';
 import { isEmpty } from 'lodash';
 
 export const adminFetchUsers = async ({ args, client, error, success }) => {
@@ -7,6 +7,27 @@ export const adminFetchUsers = async ({ args, client, error, success }) => {
     console.log("args are", args);
     const { data }: { data: { users: UserType[] } } = await client.query({
       query: GET_ALL_USERS,
+      variables: {
+        limit: 1000,
+        ...args
+      }
+    });
+
+    if (!isEmpty(data && data.users)) {
+      success(data.users);
+    }
+    throw new Error("error getting users, please try again later")
+  } catch (err) {
+    error(err)
+  }
+};
+
+export const adminFetchPaymentMethods = async ({ args, client, error, success }) => {
+  try {
+
+    console.log("args are", args);
+    const { data }: { data: { users: UserType[] } } = await client.query({
+      query: GET_PAYMENT_METHODS,
       variables: {
         limit: 1000,
         ...args
